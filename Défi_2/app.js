b98ca029edd2b796272e63118a4c6b3c17435e60
Abboudisara <57992532+Abboudisara@users.noreply.github.com>
2020-04-18 12:44:54 +0100

    réalisation-défi2

--- Défi_2/app.js
+++ Défi_2/app.js
@@ -12,5 +12,32 @@ function  loadCustomer() {
     }
     XML.open('get','customer.json')
     XML.send();
-}
+};
 
+var http = new XMLHttpRequest;
+var button2 = document.getElementById('button2');
+var customers = document.getElementById('cutomers');
+button2.addEventListener('click', function loadCustomers(){
+    http.onload = function (){
+        if (http.readyState == 4 && http.status == 200){
+            var cust = JSON.parse(http.response);
+            var output = " ";
+            for (let i = 0 ; i < cust.length; i++){
+                output +="<ul>"+
+               " <li> name:" +cust[i].name + "</li> " +
+               " <li> company:" +cust[i].company + "</li>" +
+               " <li> phone:" +cust[i].phone + "</li>" +
+               " <li>id:" +cust[i].id + "</li> " +
+
+               " </ul>";
+            }
+            document.getElementById("customers").innerHTML=output;
+        }
+        if (http.status == 404) {
+            console.log("ERROR 404")
+
+            }
+    }
+    http.open('get', 'customers.json', true);
+    http.send();
+})
